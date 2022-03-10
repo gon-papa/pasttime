92343ecf0aaaa8775f7a0a54a080969ee29b4b9d
gonda <gonda@gondanoMacBook-Air.local>
2022-03-10 23:22:18 +0900

    テストDBの設定とログインテスト作成

--- backend/tests/Feature/UserTest.php
+++ backend/tests/Feature/UserTest.php
@@ -16,23 +16,61 @@ use Tests\TestCase;
 class UserTest extends TestCase
 {
     use RefreshDatabase;
+    use WithFaker;
+
+    protected $loginUrl = 'api/v1/login';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->post('/api/v1/csrf-cookie');
+    }
 
     /** @test  */
     function ログインに成功すると200を返し認証される()
     {
         $user = User::factory()->create();
-        $this->post('/api/v1/csrf-cookie');
-        $response = $this->post('api/v1/login', [
+        $response = $this->post($this->loginUrl, [
             'email' => $user->email,
             'password' => 'password',
         ]);
         $response->assertStatus(200);
-        
     }
 
     /** @test  */
-    // function ログインに失敗すると400を返す()
-    // {
-        
-    // }
+    function ログインに失敗した場合に400を返す()
+    {
+        User::factory()->create();
+        $response = $this->post($this->loginUrl, [
+            'email' => $this->faker->email(),
+            'password' => $this->faker->password(),
+        ]);
+        $response->assertStatus(400)
+                ->assertJson([
+                    'status' => 400,
+                    'message' => 'Unauthorized',
+                ]);
+    }
+
+    /** @test  */
+    function バリデーションエラーなら400を返し、エラーメッセージを返す()
+    {
+        User::factory()->create();
+        $response = $this->post($this->loginUrl, [
+            'email' => '',
+            'password' => '',
+        ]);
+        $response->assertStatus(400);
+        $response->assertJson([
+            'status' => 400,
+            'errors' => [
+                'email' => [
+                    'メールアドレスを入力してください'
+                ],
+                'password' => [
+                    'パスワードを入力してください'
+                ]
+            ]
+        ]);
+    }
 }
